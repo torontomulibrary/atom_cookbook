111a008c924ad61190c4e37222a67a8d8e73846c
Patrick Fung <patrick@makestuffdostuff.com>
2015-05-12 15:09:53 -0400

    fold attributes for default configuration into cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,14 @@ default['atom']['database_name'] = 'atom'
 default['atom']['database_user'] = 'atom'
 default['atom']['database_user_password'] = 'atom'
 default['atom']['install_dir'] = '/var/www/html/atom'
+
+default['elasticsearch']['version'] = '1.5.2' # AtoM requires Elasticsearch version > 1.3.0
+
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = '8'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['nodejs']['npm_packages'] = [
+	{ "name" => "less" },
+	{ "name" => "gulp" },
+]
