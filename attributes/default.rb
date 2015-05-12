7317a5710b3bb5ef34e253860d10a6b75c0cb8d4
Patrick Fung <patrick@makestuffdostuff.com>
2015-05-12 15:20:05 -0400

    change java install flavour

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,8 @@ default['atom']['install_dir'] = '/var/www/html/atom'
 
 default['elasticsearch']['version'] = '1.5.2' # AtoM requires Elasticsearch version > 1.3.0
 
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk_version'] = '8'
-default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['install_flavor'] = 'openjdk' # Elasticsearch requires java
+default['java']['jdk_version'] = '7'
 
 default['nodejs']['npm_packages'] = [
 	{ "name" => "less" },
