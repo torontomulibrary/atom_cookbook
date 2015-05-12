99d7eea056b879eea6e3e3ac8820ebb69462ea53
Patrick Fung <patrick@makestuffdostuff.com>
2015-05-12 16:01:19 -0400

    Downgrade Elasticsearch version because AtoM asks for 1.3.0
    https://www.accesstomemory.org/en/docs/2.1/admin-manual/installation/linux/#installation-linux

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,11 +5,14 @@ default['atom']['database_user'] = 'atom'
 default['atom']['database_user_password'] = 'atom'
 default['atom']['install_dir'] = '/var/www/html/atom'
 
-default['elasticsearch']['version'] = '1.5.2' # AtoM requires Elasticsearch version > 1.3.0
+ # AtoM requires Elasticsearch version ~> 1.3.0
+default['elasticsearch']['version'] = '1.3.0'
 
-default['java']['install_flavor'] = 'openjdk' # Elasticsearch requires java
+# Elasticsearch requires java
+default['java']['install_flavor'] = 'openjdk' 
 default['java']['jdk_version'] = '7'
 
+# less and gulp are required to compile the .less files
 default['nodejs']['npm_packages'] = [
 	{ "name" => "less" },
 	{ "name" => "gulp" },
