d6a44a5b46712717a13be2b9b20b3bbdb6d8d638
Patrick Fung <patrick@makestuffdostuff.com>
2015-05-22 13:48:04 -0400

    Move pacakge declaration into attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,22 @@ default['atom']['install_dir'] = '/var/www/html/atom'
 default['atom']['git_repo'] = 'git://github.com/artefactual/atom.git'
 default['atom']['git_revision'] = 'stable/2.1.x'
 
+
 # should not change anything below this line unless you really
 # know what you are doing!
+
+# ImageMagick package name is different on debian/ubuntu
+default['atom']['install_packages'] = %w{
+  git
+  ImageMagick
+  ImageMagick-devel
+  ghostscript
+  poppler-utils
+  ffmpeg
+}
+
+default['atom']['php_packages'] =  %w{ php56w php56w-opcache php56w-common php56w-mysql php56w-xml php56w-ldap php56w-pecl-apcu php56w-mbstring php56w-fpm }
+
 # Elasticsearch requires Java
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
@@ -21,16 +35,8 @@ default['elasticsearch']['version'] = '1.3.0'
 default['nginx']['default_site_enabled'] = false
 default['nginx']['repo_source'] = 'nginx'
 
-default['atom']['packages'] = %w{ poppler-utils ghostscript ffmpeg git }
-case node['platform_family']
-when 'rhel', 'fedora'
-  default['atom']['packages'] += %w{ ImageMagick ImageMagick-devel }
-when 'debian', 'mac_os_x'
-  default['atom']['packages'] += %w{ imagemagick }
-end
-
 # less and gulp are required to compile the .less files
 default['nodejs']['npm_packages'] = [
   { "name" => "less" },
-  { "name" => "gulp" },
+  { "name" => "gulp" }
 ]
