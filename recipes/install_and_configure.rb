e14ed29d960ff328cb2199184f8782634ddb8b2f
Patrick Fung <patrick@makestuffdostuff.com>
2015-05-27 12:34:04 -0400

    Remove sfInstallPlugin templates

--- recipes/install_and_configure.rb
+++ recipes/install_and_configure.rb
@@ -37,21 +37,3 @@ directory "#{node['atom']['install_dir']}/uploads" do
   user "nginx"
   group "nginx"
 end
-
-# Pre-fill the web installer form with defaults
-# TODO: remove this from recipe and put into RULA/atom repository
-template "#{node['atom']['install_dir']}/plugins/sfInstallPlugin/modules/sfInstallPlugin/actions/configureSearchAction.class.php" do
-  source "configureSearchAction.class.php"
-  user "nginx"
-  group "nginx"
-end
-template "#{node['atom']['install_dir']}/plugins/sfInstallPlugin/modules/sfInstallPlugin/actions/configureDatabaseAction.class.php" do
-  source "configureDatabaseAction.class.php"
-  user "nginx"
-  group "nginx"
-end
-template "#{node['atom']['install_dir']}/plugins/sfInstallPlugin/modules/sfInstallPlugin/actions/configureSiteAction.class.php" do
-  source "configureSiteAction.class.php"
-  user "nginx"
-  group "nginx"
-end
