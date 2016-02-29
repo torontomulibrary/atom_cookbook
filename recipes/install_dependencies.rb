dc8d142703d3ed3e514ffb480debae7efc5e02fd
Patrick Fung <patrick@makestuffdostuff.com>
2016-02-29 17:19:46 -0500

    hand over php install to lits_vm cookbook

--- recipes/install_dependencies.rb
+++ recipes/install_dependencies.rb
@@ -7,36 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Enable extended repositories
-# include_recipe 'yum-epel'
-
 # Install package dependencies
-package %w(curl git gearmand)
+# package %w(curl git gearmand)
 
-# Include webtatic for EL6 releases
-if platform_family?('rhel') && node['platform_version'].to_i == 6
-  webtatic_rpm = 'https://mirror.webtatic.com/yum/el6/latest.rpm'
-  remote_file "#{Chef::Config[:file_cache_path]}/webtatic.rpm" do
-    source webtatic_rpm
-  end
-  rpm_package "#{Chef::Config[:file_cache_path]}/webtatic.rpm"
-end
+# # Include webtatic for EL6 releases
+# if platform_family?('rhel') && node['platform_version'].to_i == 6
+#   webtatic_rpm = 'https://mirror.webtatic.com/yum/el6/latest.rpm'
+#   remote_file "#{Chef::Config[:file_cache_path]}/webtatic.rpm" do
+#     source webtatic_rpm
+#   end
+#   rpm_package "#{Chef::Config[:file_cache_path]}/webtatic.rpm"
+# end
 
-# Install optional packages & dependencies
-if node['atom']['install_optional_packages']
+# # Install optional packages & dependencies
+# if node['atom']['install_optional_packages']
 
-  node['atom']['additional_repos'].each do |repo, url|
-    remote_file "#{Chef::Config[:file_cache_path]}/#{repo}.rpm" do
-      source url
-    end
-    rpm_package "#{Chef::Config[:file_cache_path]}/#{repo}.rpm"
-  end
+#   node['atom']['additional_repos'].each do |repo, url|
+#     remote_file "#{Chef::Config[:file_cache_path]}/#{repo}.rpm" do
+#       source url
+#     end
+#     rpm_package "#{Chef::Config[:file_cache_path]}/#{repo}.rpm"
+#   end
 
-  package node['atom']['optional_packages']
-end
+#   package node['atom']['optional_packages']
+# end
 
 # Install less and gulp to precomile css
-nodejs_npm 'less gulp'
+# nodejs_npm 'less gulp'
 
 # Nginx configuration for AtoM
 nginx_site 'atom' do
@@ -44,6 +41,17 @@ nginx_site 'atom' do
   notifies :reload, 'service[nginx]', :delayed
 end
 
-# MySQL, PHP
+# MySQL
 include_recipe 'atom::configure_mysql'
-include_recipe 'atom::configure_php'
+
+# PHP
+template '/etc/php.ini' do
+  source 'php.ini.erb'
+end
+
+template '/etc/php-fpm.d/atom.conf' do
+  source 'atom.php-fpm.erb'
+end
+
+
+# include_recipe 'atom::configure_php'
