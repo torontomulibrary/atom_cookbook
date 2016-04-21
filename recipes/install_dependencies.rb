f9904e17dc45713aa1fba5aebdad4d40511c2f07
Patrick Fung <patrick@makestuffdostuff.com>
2016-04-21 13:18:57 -0400

    remove nginx stuff, it's in lits_vm now!

--- recipes/install_dependencies.rb
+++ recipes/install_dependencies.rb
@@ -11,14 +11,3 @@ template "#{node['php']['fpm_pooldir']}/atom.conf" do
   source 'atom.php-fpm.erb'
   notifies :restart, 'service[php-fpm]'
 end
-
-# Nginx configuration for AtoM
-# nginx_site 'atom' do
-#   template 'atom.nginx.erb'
-# end
-
-# Force nginx to reload, it seems like nginx_site provider
-# does not do it properly
-# service 'nginx' do
-#   action :reload
-# end
