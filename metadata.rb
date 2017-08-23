7a05e797cc1aa7b4540446847fad5888ecd32fc1
Patrick Fung <patrick@makestuffdostuff.com>
2017-08-23 17:23:54 -0400

    update pin on elasticsearch cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,9 @@ maintainer_email 'patrick@makestuffdostuff.com'
 license          'All rights reserved'
 description      'Installs/Configures atom'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '0.4.2'
 
 depends 'chef_nginx', '~> 4.0.2'
-depends 'java', '~> 1.42.0'
+depends 'java', '~> 1.50.0'
 
-# AtoM lists Elasticsearch version 1.3 as a requirement.
-# https://www.accesstomemory.org/en/docs/2.3/admin-manual/installation/requirements/#installation-requirements
-# Don't use a more recen't version the elasticsearch cookbook. It's not
-# confirmed to work with the version of Elasticsearch we are targeting (1.3.x)
-# Refer to: https://github.com/elastic/cookbook-elasticsearch/tree/0.3.x
-depends 'elasticsearch', '~> 0.3.14'
+depends 'elasticsearch', '~> 2.5.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Create atom log directory
-directory node['atom']['log_dir']
-
 # Clone down AtoM into install dir
 git node['atom']['install_dir'] do
   repository node['atom']['git_repo']
